0fb2a782735b3492e71d8fb976fb93dd1686a6fd
Ryota Arai <ryota.arai@gmail.com>
2015-05-07 12:06:49 +0900

    root => revisions#index.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :plans
   resources :revisions
   post 'hooks/github' => 'hooks#github'
+  root 'revisions#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
