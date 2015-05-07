002db3afc97bf998bb5b7cb74cc1ee75c629523a
Ryota Arai <ryota-arai@cookpad.com>
2015-05-07 17:19:06 +0900

    CONSUL_HOST environment variable.

--- lib/backend/consul.rb
+++ lib/backend/consul.rb
@@ -1,13 +1,23 @@
 module Backend
   class Consul
+    def initialize
+      configure
+    end
+
     def hosts
-      Diplomat::Service.get(service_name, :all).map(&:Node)
+      Diplomat::Service.new.get(service_name, :all).map(&:Node)
     end
 
     private
 
     def service_name
       ENV.fetch('CONSUL_SERVICE')
+    end
+
+    def configure
+      Diplomat.configure do |config|
+        config.url = ENV['CONSUL_HOST'] if ENV['CONSUL_HOST'] # e.g. "http://localhost:8500"
+      end
     end
   end
 end
