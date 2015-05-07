8002abab4c1e60061a332c24d36c59e6c0f6f34b
Ryota Arai <ryota.arai@gmail.com>
2015-05-07 11:48:31 +0900

    Implementing hooks/github.

--- app/controllers/hooks_controller.rb
+++ app/controllers/hooks_controller.rb
@@ -1,4 +1,13 @@
 class HooksController < ApplicationController
   def github
+    payload = JSON.parse(request.body.string)
+
+    # git clone if not cloned yet
+    # git fetch
+    # git checkout -f rev
+    # create tarball
+    # create revision
+
+    render json: {status: "success"}
   end
 end
--- spec/requests/hooks_spec.rb
+++ spec/requests/hooks_spec.rb
@@ -2,7 +2,21 @@ require "rails_helper"
 
 RSpec.describe HooksController, type: :request do
   describe "/hooks/github" do
+    let(:payload) do
+      # https://developer.github.com/v3/activity/events/types/#pushevent
+      {
+        'ref' => 'refs/heads/apply',
+        'head_commit' => {
+          'id' => 'deadbeef',
+        },
+        'repository' => {
+          'clone_url' => 'https://github.com/baxterthehacker/public-repo.git',
+        },
+      }
+    end
+
     it "creates a new revision" do
+      post "/hooks/github", payload.to_json
     end
   end
 end
