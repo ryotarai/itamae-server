bdcffee1bb88601db4556a6f48e17053c952df6c
Ryota Arai <ryota-arai@cookpad.com>
2015-05-08 18:41:09 +0900

    Generate file path automatically.

--- app/models/log.rb
+++ app/models/log.rb
@@ -2,9 +2,21 @@ class Log < ActiveRecord::Base
   belongs_to :plan
   enum status: {pending: 0, in_progress: 1, completed: 2, aborted: 3}
 
+  before_save :set_defaults
+
   def append(text)
-    open(self.file_path, "a") do |f|
+    open(absolute_file_path, "a") do |f|
       f.write(text)
     end
+  end
+
+  private
+
+  def set_defaults
+    self.file_path ||= File.join("files", "logs", SecureRandom.uuid)
+  end
+
+  def absolute_file_path
+    Rails.root.join(self.file_path)
   end
 end
