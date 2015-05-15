e054b366398591d4c8c0386592ba96fe01e1ee24
Ryota Arai <ryota-arai@cookpad.com>
2015-05-15 14:21:56 +0900

    Add revisions.file_path column.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150507052629) do
+ActiveRecord::Schema.define(version: 20150512101053) do
 
   create_table "logs", force: :cascade do |t|
     t.string   "host",       limit: 255
@@ -36,9 +36,9 @@ ActiveRecord::Schema.define(version: 20150507052629) do
 
   create_table "revisions", force: :cascade do |t|
     t.string   "name",       limit: 255
-    t.string   "tar_url",    limit: 255
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
+    t.string   "file_path",  limit: 255
   end
 
   add_foreign_key "logs", "plans"
