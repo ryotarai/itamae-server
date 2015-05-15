ee735dda51d643271a9e4886f61c200f5346e32a
Ryota Arai <ryota-arai@cookpad.com>
2015-05-15 14:22:33 +0900

    Make Log#absolute_file_path public.

--- app/models/log.rb
+++ app/models/log.rb
@@ -10,13 +10,13 @@ class Log < ActiveRecord::Base
     end
   end
 
+  def absolute_file_path
+    Rails.root.join('public', self.file_path)
+  end
+
   private
 
   def set_defaults
     self.file_path ||= File.join("files", "logs", SecureRandom.uuid)
-  end
-
-  def absolute_file_path
-    Rails.root.join(self.file_path)
   end
 end
