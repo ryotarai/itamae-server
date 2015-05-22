57c5c2e638a2a17f2dcebf5dc35599279daef510
Ryota Arai <ryota-arai@cookpad.com>
2015-05-22 14:41:47 +0900

    Permit log.status.

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -89,6 +89,6 @@ class LogsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def log_params
-      params.require(:log).permit(:host)
+      params.require(:log).permit(:host, :status)
     end
 end
