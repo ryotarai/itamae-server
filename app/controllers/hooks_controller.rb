fb05a3e5f65fbf3744ff5d74c4388136268ed620
Ryota Arai <ryota-arai@cookpad.com>
2015-05-25 18:57:24 +0900

    Check signature of GitHub hook request.

--- app/controllers/hooks_controller.rb
+++ app/controllers/hooks_controller.rb
@@ -1,7 +1,29 @@
+require 'openssl'
+
 class HooksController < ApplicationController
+  skip_before_action :authenticate, only: [:github]
+
   def github
+    unless valid_signature?
+      render text: "Signature mismatch.", status: :unauthorized
+      return
+    end
+
     GithubWorker.perform_async(request.body.string)
 
     render json: {status: "success"}
+  end
+
+  private
+
+  def valid_signature?
+    if github_secret = ENV['GITHUB_SECRET']
+      sha1 = OpenSSL::HMAC.hexdigest(OpenSSL::Digest::Digest.new('sha1'), github_secret, request.body.string)
+      signature = "sha1=" + sha1
+
+      signature == request.headers['X-Hub-Signature']
+    else
+      true
+    end
   end
 end
