be298fbffa4f5715e797f2975f08fad40ed947bd
Ryota Arai <ryota-arai@cookpad.com>
2015-05-25 16:04:51 +0900

    If all of logs are completed, mark the plan as completed.

--- app/models/log.rb
+++ app/models/log.rb
@@ -1,8 +1,10 @@
 class Log < ActiveRecord::Base
   belongs_to :plan
+
   enum status: {pending: 0, in_progress: 1, completed: 2, aborted: 3}
 
   before_save :set_defaults
+  after_commit :update_plan_status
 
   def append(text)
     open(absolute_file_path, "a") do |f|
@@ -18,5 +20,11 @@ class Log < ActiveRecord::Base
 
   def set_defaults
     self.file_path ||= File.join("files", "logs", SecureRandom.uuid)
+  end
+
+  def update_plan_status
+    if self.plan.in_progress? && self.plan.logs.all? {|log| log.completed? }
+      self.plan.completed!
+    end
   end
 end
