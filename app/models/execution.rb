0585d9cbaed265345ebe3c12fbe91db5c741fb23
Ryota Arai <ryota-arai@cookpad.com>
2015-08-13 19:53:24 +0900

    Validate Execution#is_dry_run

--- app/models/execution.rb
+++ app/models/execution.rb
@@ -5,6 +5,7 @@ class Execution < ActiveRecord::Base
   enum status: {in_progress: 1, completed: 2, aborted: 3}
 
   validates :revision, presence: true
+  validates :is_dry_run, inclusion: [true, false]
 
   def queue
     ExecutionWorker.perform_async(self.id)
