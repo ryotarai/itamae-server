040519bb332dff76ae75c354da4801a4f6a0a37b
Ryota Arai <ryota-arai@cookpad.com>
2015-09-01 19:40:59 +0900

    Validate the revision is active before actual run.

--- app/models/execution.rb
+++ app/models/execution.rb
@@ -6,8 +6,15 @@ class Execution < ActiveRecord::Base
 
   validates :revision, presence: true
   validates :is_dry_run, inclusion: [true, false]
+  validate :validate_revision_is_active
 
   def queue
     ExecutionWorker.perform_async(self.id)
+  end
+
+  def validate_revision_is_active
+    if self.actual_run? && !self.revision.active?
+      errors.add(:revision, "Revision should be active on actual run")
+    end
   end
 end
