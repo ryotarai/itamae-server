a2ca31ee581e5a7432c068a55e311c3d6178836d
Ryota Arai <ryota-arai@cookpad.com>
2015-09-01 19:42:39 +0900

    Improve views.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,29 @@
 module ApplicationHelper
+  STATUS_LABEL_TYPE = {
+    "pending" => "default",
+    "in_progress" => "info",
+    "completed" => "success",
+    "aborted" => "danger",
+  }
+
+  def status_label(status)
+    type = STATUS_LABEL_TYPE[status]
+    unless type
+      raise "unknown status: #{status}"
+    end
+
+    %{<span class="label label-#{type}">#{status.gsub('_', ' ')}</span>}
+  end
+
+  def dry_run_label(is_dry_run)
+    if is_dry_run
+      type = "default"
+      body = "dry run"
+    else
+      type = "primary"
+      body = "actual run"
+    end
+
+    %{<span class="label label-#{type}">#{body}</span>}
+  end
 end
