4989d1b519c4f6bec1a8db955d5d2709aa90f952
Ryota Arai <ryota.arai@gmail.com>
2015-12-07 00:41:20 +0900

    Use itamae_server reporter.

--- lib/itamae_server/backend/local.rb
+++ lib/itamae_server/backend/local.rb
@@ -9,18 +9,30 @@ module ItamaeServer
       def execute(execution)
         Dir.mktmpdir do |dir|
           Dir.chdir(dir) do
+            generate_config
             url = execution.revision.url
-            sys "curl", "-L", "-o", "dest", url
-            sys "tar", "xf", "dest"
-            sys "itamae", "local", "bootstrap.rb"
+            system_or_abort "curl", "-L", "-o", "dest", url
+            system_or_abort "tar", "xf", "dest"
+            system_or_abort "itamae", "local", "-c", "config.yml", "bootstrap.rb"
           end
         end
       end
 
-      private def sys(*args)
+      private def system_or_abort(*args)
         unless system(*args)
           raise "#{args} failed"
         end
+      end
+
+      private def generate_config
+        config = {
+          'reporters' => [
+            {
+              'type' => 'itamae_server',
+            },
+          ],
+        }
+        File.write('config.yml', config.to_yaml)
       end
     end
   end
