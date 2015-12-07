a6cf10311cd58b961b31e1fd7e40e211c3df0a85
Ryota Arai <ryota.arai@gmail.com>
2015-12-07 23:39:44 +0900

    Show events with accordion.

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,4 +4,28 @@ class Event < ActiveRecord::Base
 
   validates :event_type, presence: true
   validates :host_execution, presence: true
+
+  def self.group_by_action(events)
+    result = []
+
+    recipe = nil
+    resource = nil
+
+    events.each do |event|
+      case event.event_type
+      when 'recipe_started'
+        recipe = event.payload
+      when 'resource_started'
+        resource = event.payload
+      when 'action_started'
+        result << OpenStruct.new(recipe: recipe, resource: resource, action: event.payload, events: [])
+      when /_completed\z/, /_failed\z/
+        # pass
+      else
+        result.last.events << event
+      end
+    end
+
+    result
+  end
 end
