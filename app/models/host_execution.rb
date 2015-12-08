1631a887923a635eab6e4a332e083b9ebc8675fd
Ryota Arai <ryota.arai@gmail.com>
2015-12-08 15:21:49 +0900

    Sort executions by num of updated resources.

--- app/models/host_execution.rb
+++ app/models/host_execution.rb
@@ -3,4 +3,8 @@ class HostExecution < ActiveRecord::Base
   belongs_to :execution
 
   has_many :events
+
+  def updated_resource_count
+    events.where(event_type: "resource_updated").count
+  end
 end
