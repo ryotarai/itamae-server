a54dd653738ff139a0b07abc38d0546c5c4b9a67
Ryota Arai <ryota.arai@gmail.com>
2015-12-08 15:20:46 +0900

    If allow_only_dry_run is set, actual run is not allowed.

--- app/models/execution.rb
+++ app/models/execution.rb
@@ -1,7 +1,12 @@
 class Execution < ActiveRecord::Base
   belongs_to :revision
   has_many :host_executions
+
   validates :revision, presence: true
+  if Figaro.env.allow_only_dry_run
+    validates :dry_run, inclusion: {in: [true], message: ": Actual run is not allowed."}
+  end
+
   after_create :enqueue_job
 
   private def enqueue_job
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151207111246) do
+ActiveRecord::Schema.define(version: 20151208052708) do
 
   create_table "events", force: :cascade do |t|
     t.string   "event_type",        limit: 255
@@ -25,9 +25,9 @@ ActiveRecord::Schema.define(version: 20151207111246) do
 
   create_table "executions", force: :cascade do |t|
     t.integer  "revision_id", limit: 4
-    t.datetime "created_at",            null: false
-    t.datetime "updated_at",            null: false
-    t.boolean  "dry_run"
+    t.datetime "created_at",                           null: false
+    t.datetime "updated_at",                           null: false
+    t.boolean  "dry_run",               default: true
   end
 
   add_index "executions", ["revision_id"], name: "index_executions_on_revision_id", using: :btree
